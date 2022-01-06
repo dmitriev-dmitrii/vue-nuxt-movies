598ba704df336428ff91e5c38d6dbeaee463f6b0
dmitriev dmitrii <dmitriev.dmitrii@mail.com>
2022-01-06 23:01:49 +0300

    save

--- components/loadingSpinner.vue
+++ components/loadingSpinner.vue
@@ -1,5 +1,5 @@
 <template>
-<span class="inline-flex justify-center items-center"> <div class="my-spiner mr-1"></div><slot></slot></span>
+<div class="inline-flex justify-center items-center"> <div class="my-spiner mr-1 "></div><slot></slot></div>
 </template>
 
 <script>
@@ -17,18 +17,20 @@ width: .8em;
 	display: inline-block;
 	border-radius: 50%;
 
-	border:.1em solid rgba(0, 220, 128, 0.4);
-	border-top: .1em solid #00DC82;
+	border:.1em solid rgba(0, 220, 128, 0.1); 
+	border-top: .1em solid   #00DC82;
 	animation: spiner 1.7s linear infinite;
 	
 }
 @keyframes spiner {
 	0%   { 
 		transform: rotate(0deg) ;
+
 	}
 
 	100%  { 
 		transform: rotate(-360deg) ;
+
 	}
 }
 </style>
---
+++ store/search.js
@@ -0,0 +1,76 @@
+import axios from 'axios';
+import api from "@/api/api";
+
+export const state = () => ({
+
+    person: {
+        images: [],
+        moviesList:''
+    },
+
+})
+
+export const getters = {
+
+    getPerson: (state) => {
+        return state.person;
+    },
+
+}
+
+export const mutations = {
+
+    mutatePerson: (state, obj) => {
+        Object.assign(state.person, obj);
+    },
+
+    mutatePersonMoviesList: (state, data) => {
+
+        data.cast.sort(function (a, b) {
+            if (a.popularity < b.popularity) {
+            return 1;
+            }
+            if (a.popularity > b.popularity) {
+            return -1;
+            }
+            // a должно быть равным b
+            return 0;
+        });
+
+        const obj = {
+            moviesList: data
+        }
+        Object.assign(state.person, obj);
+        
+    },
+    
+}
+
+export const actions = {
+
+    axiosPerson: async (context, personId) => {
+        
+        const request = await axios.get(
+            `${api.url}/person/${personId}?${api.key}&language=${context.rootState.language}&`
+        );
+        if (request.status == 200) {
+            // console.log( request.data);
+            // console.log(context);
+            context.commit('mutatePerson', request.data);
+        }
+    },
+
+    axiosPersonMoviesList: async (context, personId) => {
+
+        const request = await axios.get(
+            `${api.url}/person/${personId}/movie_credits?${api.key}&language=${context.rootState.language}&`
+        );
+        if (request.status == 200) {
+            // console.log( request.data);
+            // console.log(context);
+            context.commit('mutatePersonMoviesList', request.data);
+        }
+    },
+
+
+}
