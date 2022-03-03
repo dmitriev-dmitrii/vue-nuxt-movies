4d6c3cf18190f21c4c86f11cba9fa944a6ae0a9b
dmitriev dmitrii <dmitriev.dmitrii@mail.com>
2022-03-03 15:10:47 +0300

    bug

--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -1,16 +1,16 @@
 <template>
-<div class="" style="content-visibility:auto;">
+<div >
 	
 	<h1 class=" font-bold text-3xl md:text-4xl lg:text-5xl font-heading ">
 		<span-ru-en ru="Фильмы" en="Movies"/>
 	</h1>
 
 	<div class="flex flex-wrap mb-4 mt-4">
-		<movies-types-input v-for=" movieType  in this.$store.state.movies.moviesTypes" :key="movieType.name" :movieType='movieType.name' />
-	</div>
+		<movies-types-input v-for=" movieType  in this.$store.state.movies.list.moviesTypes" :key="movieType.name" :movieType='movieType.name' />
+	</div> 
 	
 	<div>
-		<div v-for="movieType , index in this.$store.state.movies.moviesTypes" :key="movieType.name" :class="{ hidden : movieType.name !== currentMoviesType }"  >
+		<div v-for="movieType , index in this.$store.state.movies.list.moviesTypes" :key="movieType.name" :class="{ hidden : movieType.name !== currentMoviesType }"  >
 			
 			<div class="grid grid-cols-1 gap-4  sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6" >
 
@@ -23,7 +23,7 @@
 				</div>
 			</div>
 		</div>
-</div>
+	</div>
 
 </div>
 
@@ -55,7 +55,7 @@ methods: {
 
 	async fetch (context) {
 
-		for (let index = 0; index < context.store.state.movies.moviesTypes.length; index++) {
+		for (let index = 0; index < context.store.state.movies.list.moviesTypes.length; index++) {
 			await context.store.dispatch('movies/list/axiosMovies', [index] )
 		}
 
